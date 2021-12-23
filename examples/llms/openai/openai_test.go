645c328dae75e9c70e7d7e959092c3a93824cbd7
say-aloha <167399741+say-aloha@users.noreply.github.com>
2021-12-23 12:01:35 +0000

    fix setting

--- examples/llms/openai/openai_test.go
+++ examples/llms/openai/openai_test.go
@@ -16,3 +16,19 @@ func TestBasicCompletion(t *testing.T) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	completion, err := llm.Call(context.Background(), "Question, what kind of bear is best?", []string{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(completion)
+}
+
+func TestBasicCompletionWithStop(t *testing.T) {
+	llm, err := openai.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	completion, err := llm.Call(context.Background(), "Question, what kind of bear is best?", []string{"bear"})
+	if err != nil {
+		log.Fatal(err)

ac757523fd84b89f6d02d1e01341d41e20a025af
say-aloha <167399741+say-aloha@users.noreply.github.com>
2021-12-23 18:19:01 +0000

    Update depends

--- examples/llms/openai/openai_test.go
+++ examples/llms/openai/openai_test.go
@@ -32,3 +32,14 @@ func TestBasicCompletionWithStop(t *testing.T) {
 	completion, err := llm.Call(context.Background(), "Question, what kind of bear is best?", []string{"bear"})
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	fmt.Println(completion)
+}
+
+func TestBatchCompletion(t *testing.T) {
+	llm, err := openai.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	completion, err := llm.Generate(context.Background(), []string{

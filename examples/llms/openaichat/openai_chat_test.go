1068ea22180c39672f52e007d7c1ed85365c155a
say-aloha <167399741+say-aloha@users.noreply.github.com>
2022-01-01 05:53:40 +0000

    Update abstract

--- examples/llms/openaichat/openai_chat_test.go
+++ examples/llms/openaichat/openai_chat_test.go
@@ -5,3 +5,21 @@ import (
 	"context"
 	"fmt"
 	"github.com/speakeasy-api/langchain-go/llms/openaichat"
+	"log"
+	"testing"
+)
+
+// To Execute EXPORT OPENAI_API_KEY=...
+
+func TestFirstMessageChat(t *testing.T) {
+	llm, err := openaichat.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	completion, err := llm.Call(context.Background(), "Hi, how are you?", []string{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(completion)
+}

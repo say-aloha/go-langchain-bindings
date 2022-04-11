0f08b72beb382a9fc59001f2376d7c5adc1c549a
say-aloha <167399741+say-aloha@users.noreply.github.com>
2022-04-11 17:06:51 +0000

    Upgraded nokogiri gem

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -7,4 +7,4 @@ import (
 
 	openai_shared "github.com/speakeasy-api/langchain-go/llms/shared/openai"
 	gpt "github.com/speakeasy-sdks/openai-go-sdk"
-	"github.com/speakeasy-sdks/openai-go-s
+	"github.com/speakeasy-sdks/openai-go-sdk/pkg/models/sh

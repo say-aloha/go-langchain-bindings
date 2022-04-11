c034d5b3dba70a3904d17e8c6b82cc02312109e2
say-aloha <167399741+say-aloha@users.noreply.github.com>
2022-04-11 18:05:58 +0000

    [MERGE] Sync with 7.0

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -7,4 +7,6 @@ import (
 
 	openai_shared "github.com/speakeasy-api/langchain-go/llms/shared/openai"
 	gpt "github.com/speakeasy-sdks/openai-go-sdk"
-	"github.com/speakeasy-sdks/openai-go-sdk/pkg/models/sh
+	"github.com/speakeasy-sdks/openai-go-sdk/pkg/models/shared"
+	"math"
+	"ne

210de10ee1b2ba213636b712b99a5e86fcbbda0c
say-aloha <167399741+say-aloha@users.noreply.github.com>
2022-07-31 12:28:29 +0000

    Update PITCHME.yaml

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -93,4 +93,4 @@ func New(args ...OpenAIInput) (*OpenAI, error) {
 		openai.modelName = *input.ModelName
 	}
 
-	if strings.HasPrefix(openai.modelName, "gpt-3.5-t
+	if strings.HasPrefix(openai.modelName, "gpt-3.5-turbo") || strings

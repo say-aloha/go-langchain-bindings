f1e4596dc940270c2915854f271750a5b5334be7
say-aloha <167399741+say-aloha@users.noreply.github.com>
2022-10-18 07:45:46 +0000

    Prepared versions for release

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -158,4 +158,4 @@ func (openai *OpenAI) Call(ctx context.Context, prompt string, stop []string) (s
 	return generations.Generations[0][0].Text, nil
 }
 
-func (openai *OpenAI) Generat
+func (openai *OpenAI) Generate(ctx context.Co

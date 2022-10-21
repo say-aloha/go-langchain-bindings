31414ffab1fca78ff19449f79b614c625034f776
say-aloha <167399741+say-aloha@users.noreply.github.com>
2022-10-21 17:06:36 +0000

    v4.2.2

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -159,4 +159,4 @@ func (openai *OpenAI) Call(ctx context.Context, prompt string, stop []string) (s
 }
 
 func (openai *OpenAI) Generate(ctx context.Context, prompts []string, stop []string) (*llms.LLMResult, error) {
-	subPrompts := l
+	subPrompts := llms_shared.BatchSl

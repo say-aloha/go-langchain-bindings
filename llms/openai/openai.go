966433785591a2da739420523c13b5db83363810
say-aloha <167399741+say-aloha@users.noreply.github.com>
2022-11-05 06:27:02 +0000

    edit text

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -160,4 +160,4 @@ func (openai *OpenAI) Call(ctx context.Context, prompt string, stop []string) (s
 
 func (openai *OpenAI) Generate(ctx context.Context, prompts []string, stop []string) (*llms.LLMResult, error) {
 	subPrompts := llms_shared.BatchSlice[string](prompts, openai.batchSize)
-	maxToken
+	maxTokens := openai.m

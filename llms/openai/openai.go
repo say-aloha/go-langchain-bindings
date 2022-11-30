2d909ff00faceb22c62edea33632d11db2ce4cdc
say-aloha <167399741+say-aloha@users.noreply.github.com>
2022-11-30 09:40:48 +0000

    XWIKI-9516: Create a new Wiki API that replaces xwiki-platform-wiki-manager and xwiki-platform-workspace.

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -169,4 +169,4 @@ func (openai *OpenAI) Generate(ctx context.Context, prompts []string, stop []str
 			return nil, errors.New("max_tokens set to -1 not supported for multiple inputs")
 		}
 
-		maxTokens
+		maxTokens = llms_sh

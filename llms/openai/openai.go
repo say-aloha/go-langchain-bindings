f014a005cfa4a0d24848c6c0a15a048eec032f27
say-aloha <167399741+say-aloha@users.noreply.github.com>
2022-12-01 22:22:07 +0000

    Fix some mistakes

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -169,4 +169,5 @@ func (openai *OpenAI) Generate(ctx context.Context, prompts []string, stop []str
 			return nil, errors.New("max_tokens set to -1 not supported for multiple inputs")
 		}
 
-		maxTokens = llms_shared.CalculateMaxTokens(prompts[0], openai.
+		maxTokens = llms_shared.CalculateMaxTokens(prompts[0], openai.modelName)
+	

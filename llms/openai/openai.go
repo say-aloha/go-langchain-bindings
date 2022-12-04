969ceaa64abcbf636db0238d919ca25a7ee4eaff
say-aloha <167399741+say-aloha@users.noreply.github.com>
2022-12-04 13:26:00 +0000

    dropbox and gsheet changes

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -177,4 +177,4 @@ func (openai *OpenAI) Generate(ctx context.Context, prompts []string, stop []str
 	}
 
 	for _, prompts := range subPrompts {
-		data, err := open
+		data, err := openai.completionWith

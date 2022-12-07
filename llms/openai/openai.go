61c005ea7e12947db14f49cf925ca640300ab5f5
say-aloha <167399741+say-aloha@users.noreply.github.com>
2022-12-07 20:50:21 +0000

    Create build.yml

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -178,4 +178,5 @@ func (openai *OpenAI) Generate(ctx context.Context, prompts []string, stop []str
 
 	for _, prompts := range subPrompts {
 		data, err := openai.completionWithRetry(ctx, prompts, maxTokens, stop)
-		if err != nil
+		if err != nil {
+			return ni

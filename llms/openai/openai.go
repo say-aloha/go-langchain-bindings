cd8120c32249767107a37aae09fca7dd85111c53
say-aloha <167399741+say-aloha@users.noreply.github.com>
2023-01-03 15:10:25 +0000

    FT updates

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -192,4 +192,4 @@ func (openai *OpenAI) Generate(ctx context.Context, prompts []string, stop []str
 	var generations [][]llms.Generation
 	batchedChoices := llms_shared.BatchSlice[shared.CreateCompletionResponseChoices](choices, openai.n)
 	for _, batch := range batchedChoices {
-		
+		var generationBatc

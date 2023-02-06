dea73b49ed869766476c8c8cc78845920a9ecd30
say-aloha <167399741+say-aloha@users.noreply.github.com>
2023-02-06 15:39:21 +0000

    fix missing ;

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -198,4 +198,6 @@ func (openai *OpenAI) Generate(ctx context.Context, prompts []string, stop []str
 				Text: *choice.Text,
 				GenerationInfo: map[string]interface{}{
 					"finishReason": choice.FinishReason,
-					"logprobs":     choice.Logpro
+					"logprobs":     choice.Logprobs,
+				},
+			})

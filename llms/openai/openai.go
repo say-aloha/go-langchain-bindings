8379a8a9fa39c66ebc68170cff4fcd32e8e16410
say-aloha <167399741+say-aloha@users.noreply.github.com>
2023-04-16 18:53:49 +0000

    Cosmetic

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -215,4 +215,4 @@ func (openai *OpenAI) Generate(ctx context.Context, prompts []string, stop []str
 	}, nil
 }
 
-func (openai *OpenAI) completionWithRetry(ctx context.Context, prompts []strin
+func (openai *OpenAI) completionWithRetry(ctx context.Context, prompts []string, maxTokens int6

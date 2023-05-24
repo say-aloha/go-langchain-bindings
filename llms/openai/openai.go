df2c3e420302700865fd2a88882dbc75d416d63f
say-aloha <167399741+say-aloha@users.noreply.github.com>
2023-05-24 17:02:15 +0000

    debug mode

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -218,4 +218,5 @@ func (openai *OpenAI) Generate(ctx context.Context, prompts []string, stop []str
 func (openai *OpenAI) completionWithRetry(ctx context.Context, prompts []string, maxTokens int64, stop []string) (*shared.CreateCompletionResponse, error) {
 	promptRequest := shared.CreateCreateCompletionRequestPromptArrayOfstr(prompts)
 	request := shared.CreateCompletionRequest{
-		Model:            open
+		Model:            openai.modelName,
+		Pr

e7c3e9c3391a8a57386369bfcbfb9619249ec034
say-aloha <167399741+say-aloha@users.noreply.github.com>
2023-11-20 22:03:55 +0000

    merge upstream

--- llms/openai/openai.go
+++ llms/openai/openai.go
@@ -260,4 +260,4 @@ func (openai *OpenAI) completionWithRetry(ctx context.Context, prompts []string,
 			finalResult = res.CreateCompletionResponse
 			break
 		} else {
-			openAIError := openai_shared.CreateOpenAIError(res.Statu
+			openAIError := openai_shared.CreateOpenAIError(res.StatusCode, res.RawRe

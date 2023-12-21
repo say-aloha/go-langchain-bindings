fa90b875f399ba03e972c3ea5cf2468783f2d27d
say-aloha <167399741+say-aloha@users.noreply.github.com>
2023-12-21 16:59:03 +0000

    add weinre

--- llms/openaichat/openai_chat.go
+++ llms/openaichat/openai_chat.go
@@ -10,3 +10,22 @@ import (
 	"math"
 	"net"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/speakeasy-api/langchain-go/llms"
+)
+
+// Default Params for Open AI model
+const (
+	temperature      float64 = 1
+	topP             float64 = 1
+	frequencyPenalty float64 = 0
+	presencePenalty  float64 = 0
+	n                int64   = 1
+	modelName        string  = "gpt-3.5-turbo"
+	maxRetries       int     = 3
+)
+
+type OpenAIChat struct {
+	temperature      float64

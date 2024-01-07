b3bc742a82458a90537817f23a7317e66ccc8f86
say-aloha <167399741+say-aloha@users.noreply.github.com>
2024-01-07 22:17:17 +0000

    added yarn.lock

--- llms/openaichat/types.go
+++ llms/openaichat/types.go
@@ -1,2 +1,18 @@
 
 package openaichat
+
+import (
+	"github.com/speakeasy-api/langchain-go/llms/openai"
+)
+
+type OpenAIChatInput struct {
+	// ChatGPT messages to pass as a prefix to the prompt
+	PrefixMessages []ChatMessage
+
+	openai.OpenAIInput
+}
+
+type ChatMessage struct {
+	Content string
+	Role    ChatMessageRoleEnum
+}

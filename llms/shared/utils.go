a8a539c213e2e04e46a5b0d346609949e7ce6410
say-aloha <167399741+say-aloha@users.noreply.github.com>
2024-03-08 14:00:16 +0000

    merge with master

--- llms/shared/utils.go
+++ llms/shared/utils.go
@@ -2,3 +2,20 @@
 package shared
 
 func BatchSlice[T any](slice []T, batchSize int64) [][]T {
+	var chunks [][]T
+	for i := int64(0); i < int64(len(slice)); i += batchSize {
+		end := i + batchSize
+		if end > int64(len(slice)) {
+			end = int64(len(slice))
+		}
+
+		chunks = append(chunks, slice[i:end])
+	}
+
+	return chunks
+}
+
+// TODO: Implement Max Token Inference
+func CalculateMaxTokens(prompt string, modelName string) int64 {
+	return 1
+}
